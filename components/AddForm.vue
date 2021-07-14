23503b67cf645a9e2b7324ea5839d18e73072b14
Julio Aguilar <julio.aguilar9928@gmail.com>
2021-07-14 09:29:36 -0500

    🦉 create add employee form

---
+++ components/AddForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <form class="flex flex-col gap-2 p-5">
+    <label class="flex shadow w-3/12 gap-3 p-2">
+      <span>Name:</span>
+      <input type="text" class="bg-transparent outline-none" />
+    </label>
+    <label class="flex justify-between shadow w-3/12 gap-3 p-2">
+      <span>Employee type:</span>
+      <select
+        id="employee_type"
+        v-model="selectedOption"
+        class="bg-transparent"
+      >
+        <option v-for="option in options" :key="option" :value="option">
+          {{ option }}
+        </option>
+      </select>
+    </label>
+    <button
+      type="button"
+      class="w-32 bg-gray-200 p-2 shadow"
+      @click.prevent="onAddEmployee()"
+    >
+      Add
+    </button>
+  </form>
+</template>
+
+<script>
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const options = ['Manager', 'QA Tester', 'Developer']
+    const selectedOption = ref(options[0])
+
+    const onAddEmployee = () => {}
+
+    return {
+      options,
+      selectedOption,
+      onAddEmployee,
+    }
+  },
+})
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,22 @@
 <template>
-  <div></div>
+  <div class="container mx-auto mt-16 main w-5/6 bg-gray-50 shadow">
+    <AddForm />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
+import AddForm from '@/components/AddForm.vue'
 
 export default defineComponent({
+  components: { AddForm },
   layout: 'main',
   setup() {},
 })
 </script>
+
+<style scoped>
+.main {
+  height: 600px;
+}
+</style>
