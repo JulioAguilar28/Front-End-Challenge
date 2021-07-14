8785cefd7a6f0d9d66aced5e82f31c4de69027fa
Julio Aguilar <julio.aguilar9928@gmail.com>
2021-07-13 21:38:33 -0500

    🎨 add tailwind colors and create main layout

--- components/TheHearder.vue
+++ components/TheHearder.vue
@@ -1,12 +1,15 @@
 <template>
-  <div></div>
+  <header>
+    <h1 class="w-2/5 pl-10 pt-5 text-2xl">Bunsan Cloud</h1>
+  </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
-  layout: 'main',
   setup() {},
 })
 </script>
+
+<style></style>
---
+++ layouts/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-gray-100 w-screen h-screen">
+    <TheHeader />
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import TheHeader from '@/components/TheHearder.vue'
+
+export default defineComponent({
+  components: { TheHeader },
+  setup() {},
+})
+</script>
+
+<style></style>
