ce7f91da682330f511439d9d516aae5bd89c0fb4
Julio Aguilar <julio.aguilar9928@gmail.com>
2021-07-20 12:30:49 -0500

    💣 Change index page component

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,75 @@
 <template>
   <div class="mx-auto mt-8 w-5/6 main bg-gray-50 shadow p-5">
     <AddForm v-if="state.showAddForm" class="mb-10" />
-    <div v-if="department.manager.name">
-      <Employee :employee="department.manager" />
+    <div v-if="state.department.manager.name">
+      <Employee :employee="state.department.manager" />
     </div>
 
-    <div>Total: ${{ departmentTotalAccount }}</div>
+    <div>Total: ${{ state.departmentTotalAccount }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, useStore, ref, watch } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  watch,
+  reactive,
+  computed,
+} from '@nuxtjs/composition-api'
 import AddForm from '@/components/AddForm.vue'
 import Employee from '@/components/Employee.vue'
-import { accessorType } from '~/store'
 import { Manager } from '~/models'
+import { buildComponentContext } from '~/types/componentContex'
+
+interface IndexState {
+  total: number
+  departmentTotalAccount: number
+  department: any
+}
 
 export default defineComponent({
   components: { AddForm, Employee },
   layout: 'main',
   setup() {
-    const store = useStore<typeof accessorType>()
-    const department = ref(store.state.department)
-    const total = ref(0)
-    const departmentTotalAccount = ref(0)
-
-    const getAmount = (manager: Manager) => {
-      let amount = 0
-      if (manager.nodes) {
-        amount = manager.type.asignment
-        iterateAmount(manager)
-        amount += total.value
-      }
-      total.value = 0
-      return amount
-    }
-
-    const iterateAmount = (manager: Manager) => {
-      for (let i = 0; i < manager.nodes.length; i++) {
-        total.value += manager.nodes[i].type.asignment
-        if (manager.nodes[i].nodes) {
-          iterateAmount(manager.nodes[i])
-        }
-      }
-    }
-
-    watch(department.value, () => {
-      if (department.value.manager.name) {
-        departmentTotalAccount.value = getAmount(department.value.manager)
-      } else {
-        departmentTotalAccount.value = 0
-      }
+    const context = buildComponentContext()
+    const state: IndexState = reactive({
+      total: 0,
+      departmentTotalAccount: 0,
+      department: computed(() => context.$accessor.department),
+      showAddForm: computed(() => context.$accessor.showAddForm),
     })
 
-    return {
-      department,
-      state: store.state,
-      total,
-      departmentTotalAccount,
+    const getAmountHandler = () => {
+      state.departmentTotalAccount = state.department.manager.name
+        ? getAmount(state, state.department.manager)
+        : 0
     }
+
+    watch(state.department, getAmountHandler)
+
+    return { state }
   },
 })
+
+const getAmount = (state: IndexState, manager: Manager): number => {
+  let amount = 0
+  if (manager.nodes) {
+    amount = manager.type.asignment
+    iterateAmount(state, manager)
+    amount += state.total
+  }
+  state.total = 0
+  return amount
+}
+
+const iterateAmount = (state: IndexState, manager: Manager) => {
+  for (let i = 0; i < manager.nodes.length; i++) {
+    state.total += manager.nodes[i].type.asignment
+    if (manager.nodes[i].nodes) {
+      iterateAmount(state, manager.nodes[i])
+    }
+  }
+}
 </script>
 
 <style scoped>
